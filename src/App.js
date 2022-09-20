6235e6d4dcee710fdd0c7efe97bf97983ac857aa
Paul Scorti <scortip@gmail.com>
2022-09-20 08:08:48 +0000

    complete React Props section

--- src/App.js
+++ src/App.js
@@ -10,24 +10,7 @@ function getTitle(title) {
 }
 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comment: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://reduc.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comment: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
+
 
 
 const App = () => {
@@ -36,21 +19,67 @@ const App = () => {
     console.log(event.target.value)
   }
 
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comment: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://reduc.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comment: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ];
+
+  const shopItems = [
+    {
+      item: 'Milk',
+      quantity: 23,
+      unitPrice: 34,
+      sellingPrice: 36,
+      objectID: 0
+    },
+    {
+      item: 'Milo',
+      quantity: 60,
+      unitPrice: 29,
+      sellingPrice: 33,
+      objectID: 1
+    },
+    {
+      item: 'Hollandia',
+      quantity: 50,
+      unitPrice: 18,
+      sellingPrice: 20,
+      objectID: 2
+    }
+  ]
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" onChange={handleChange} />
 
-      <hr></hr>
 
-      <List></List>
+      <List list={stories} />
+
+      <hr />
+
+      <Shop shop={shopItems} />
     </div>
   );
 }
 
-const List = () =>
-  list.map(item => (
+const List = props =>
+  props.list.map(item => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -62,5 +91,14 @@ const List = () =>
   ));
 
 
+const Shop = props =>
+  props.shop.map(item => (
+    <div key={item.objectID}>
+      <div>Item: {item.item}</div>
+      <div>quantity: {item.quantity}</div>
+      <div>Unit Price: {item.unitPrice} </div>
+      <div>Selling Price: {item.sellingPrice} </div>
+    </div>
+  ))
 
 export default App;
